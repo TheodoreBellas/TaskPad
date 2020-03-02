4f47a4ff6b205bc0df882783b43c66d648dcf7c8
Theodore Bellas <theodore.bellas@gmail.com>
2020-03-02 04:33:57 -0500

    Update guest root view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h4>Welcome to TaskPad!</h4>
+                    <small>Project Management Made Simple</small>
+                </div>
+                <div class="card-body">
+                    To get started, <a href="{{ route('register') }}">{{ __('register an account') }}</a>
+                    or <a href="{{ route('login') }}">{{ __('log in') }}</a>!
 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endSection
