6c3e50897e88e476effa31b762385b404e2e6da8
Theodore Bellas <theodore.bellas@gmail.com>
2020-03-03 10:06:18 -0500

    Update Task model to use belongsTo instead of hasOne for project relation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -33,7 +33,7 @@ class Task extends Model
      */
     public function project()
     {
-        return $this->hasOne(Project::class);
+        return $this->belongsTo(Project::class);
     }
 
     /**
